e5ca7085c2970b6f33872f2f0c25ffff1f88dc4f
Brian Fan and Peter Karellas <brian@>
2017-04-03 14:04:04 -0700

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //business logic
-function Places(city, landmark, population) {
+function Places(city, landmark, population, date) {
   this.city = city;
   this.landmark = landmark;
   this.population = population;
+  this.date = date;
 }
 Places.prototype.upper = function(){
   return this.city.toUpperCase();
+
+}
+
+var dog= function(){
+  $("input#city").val("");
+  $("input#landmark").val("");
+  $("input#population").val("");
+  $("input#dates").val("");
 }
 
 // user interface logic
@@ -16,8 +25,9 @@ $(document).ready(function() {
     var inputCity = $("input#city").val();
     var inputLandmark = $("input#landmark").val();
     var inputPopulation = $("input#population").val();
+    var inputDate = $("input#dates").val();
 
-    var newCity = new Places(inputCity, inputLandmark, inputPopulation);
+    var newCity = new Places(inputCity, inputLandmark, inputPopulation, inputDate);
 
     $("ul#cities").append("<li><span class='each-city'>" + newCity.upper()+ "</span></li>");
 
@@ -26,9 +36,9 @@ $(document).ready(function() {
   $("#show-city h2").text(newCity.city);
   $(".landmark").text(newCity.landmark);
   $(".population").text(newCity.population);
+  $(".date").text(newCity.date);
 });
-    $("input#city").val("");
-    $("input#landmark").val("");
-    $("input#population").val("");
+  dog();
+
   });
 });
